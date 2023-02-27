2304fc9f526f831f2c2a4bc1411fdf2c4c284871
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2023-02-27 20:32:55 +0200

    Alpha 1.2 Missing 'Stars' for updating score Not fully working NPC Bad FPS

--- 2DGame/Form1.Designer.cs
+++ 2DGame/Form1.Designer.cs
@@ -33,8 +33,19 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.player = new System.Windows.Forms.PictureBox();
             this.BORDER = new System.Windows.Forms.PictureBox();
+            this.BORDER2 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.BORDER3 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.BORDER_SIGN_RIGHT = new System.Windows.Forms.PictureBox();
+            this.BORDER_SIGN_LEFT = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BORDER)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER_SIGN_RIGHT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER_SIGN_LEFT)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -47,7 +58,7 @@
             // 
             this.player.BackColor = System.Drawing.Color.Transparent;
             this.player.Image = global::_2DGame.Resource1.USERcar;
-            this.player.Location = new System.Drawing.Point(592, 576);
+            this.player.Location = new System.Drawing.Point(642, 580);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(100, 200);
             this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -57,21 +68,83 @@
             // 
             // BORDER
             // 
-            this.BORDER.Location = new System.Drawing.Point(12, 991);
+            this.BORDER.Location = new System.Drawing.Point(47, 1150);
             this.BORDER.Name = "BORDER";
             this.BORDER.Size = new System.Drawing.Size(994, 33);
             this.BORDER.TabIndex = 5;
             this.BORDER.TabStop = false;
             this.BORDER.Tag = "border";
+            this.BORDER.Click += new System.EventHandler(this.BORDER_Click);
+            // 
+            // BORDER2
+            // 
+            this.BORDER2.Location = new System.Drawing.Point(41, 1600);
+            this.BORDER2.Name = "BORDER2";
+            this.BORDER2.Size = new System.Drawing.Size(927, 11);
+            this.BORDER2.TabIndex = 6;
+            this.BORDER2.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 30;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // BORDER3
+            // 
+            this.BORDER3.Location = new System.Drawing.Point(12, 2500);
+            this.BORDER3.Name = "BORDER3";
+            this.BORDER3.Size = new System.Drawing.Size(1036, 21);
+            this.BORDER3.TabIndex = 7;
+            this.BORDER3.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // BORDER_SIGN_RIGHT
+            // 
+            this.BORDER_SIGN_RIGHT.Location = new System.Drawing.Point(727, 1110);
+            this.BORDER_SIGN_RIGHT.Name = "BORDER_SIGN_RIGHT";
+            this.BORDER_SIGN_RIGHT.Size = new System.Drawing.Size(203, 10);
+            this.BORDER_SIGN_RIGHT.TabIndex = 8;
+            this.BORDER_SIGN_RIGHT.TabStop = false;
+            // 
+            // BORDER_SIGN_LEFT
+            // 
+            this.BORDER_SIGN_LEFT.Location = new System.Drawing.Point(197, 1110);
+            this.BORDER_SIGN_LEFT.Name = "BORDER_SIGN_LEFT";
+            this.BORDER_SIGN_LEFT.Size = new System.Drawing.Size(157, 16);
+            this.BORDER_SIGN_LEFT.TabIndex = 9;
+            this.BORDER_SIGN_LEFT.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Constantia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(39, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 28);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Score:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::_2DGame.Resource1.Backroad;
-            this.ClientSize = new System.Drawing.Size(1027, 801);
-            this.Controls.Add(this.BORDER);
+            this.ClientSize = new System.Drawing.Size(1103, 848);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BORDER_SIGN_LEFT);
+            this.Controls.Add(this.BORDER_SIGN_RIGHT);
+            this.Controls.Add(this.BORDER3);
+            this.Controls.Add(this.BORDER2);
             this.Controls.Add(this.player);
+            this.Controls.Add(this.BORDER);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -84,7 +157,12 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BORDER)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER_SIGN_RIGHT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BORDER_SIGN_LEFT)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,5 +171,12 @@
         private System.Windows.Forms.Timer timer1;
         private PictureBox player;
         private PictureBox BORDER;
+        private PictureBox BORDER2;
+        private System.Windows.Forms.Timer timer2;
+        private PictureBox BORDER3;
+        private System.Windows.Forms.Timer timer3;
+        private PictureBox BORDER_SIGN_RIGHT;
+        private PictureBox BORDER_SIGN_LEFT;
+        private Label label1;
     }
 }
--- 2DGame/Form1.cs
+++ 2DGame/Form1.cs
@@ -18,20 +18,54 @@ namespace _2DGame
     public partial class Form1 : Form
     {
         bool moveRight, moveLeft;
-        int speed = 20, enmspeed = 1;
+        int speed = 20, enmspeed = 1, score = 0;
         bool gameOver = true;
         int amount_enm = 0;
+        int amount_signL = 0, amount_signR = 0;
         Random rand = new Random();
         List<PictureBox> enemys = new List<PictureBox>();
-        
+        List<PictureBox> marksL = new List<PictureBox>();
+        List<PictureBox> marksR = new List<PictureBox>();
+
+
+
+
+
 
         public Form1()
         {
+            
             InitializeComponent();
-            this.BackgroundImage = Resource1.Backroad;
+            this.BackgroundImage = Resource1.Background;
             this.BackgroundImageLayout = ImageLayout.Stretch;
         }
 
+        private void makeMarkL()
+        {
+            PictureBox newMarkl = new PictureBox();
+            newMarkl.Height = 100;
+            newMarkl.Width = 10;
+            newMarkl.BackColor = Color.WhiteSmoke;
+            newMarkl.Tag = "Marking";
+            newMarkl.Location= new Point(270, -70);
+            newMarkl.SendToBack();
+            this.Controls.Add(newMarkl);
+            marksL.Add(newMarkl);
+            amount_signL++;
+        }
+        private void makeMarkR()
+        {
+            PictureBox newMarkR = new PictureBox();
+            newMarkR.Height = 100;
+            newMarkR.Width = 10;
+            newMarkR.BackColor = Color.WhiteSmoke;
+            newMarkR.Tag = "Marking";
+            newMarkR.Location = new Point(825, -70);
+            newMarkR.SendToBack();
+            this.Controls.Add(newMarkR);
+            marksR.Add(newMarkR);
+            amount_signR++;
+        }
         private void MakeEnemys()
         {
             PictureBox newEnemy = new PictureBox();
@@ -39,15 +73,16 @@ namespace _2DGame
             newEnemy.Width = 100;
             newEnemy.Image = Resource1.NPCcar;
             newEnemy.Tag = "Enemy";
+            newEnemy.BringToFront();
             int x=0;
             
 
-            int pos = rand.Next(1,4);
-            if (pos == 1) { x = 85; } else if (pos == 2) { x = 330; } else if (pos == 3) { x = 600; } else if(pos == 4) { x = 850; }
+            int pos = rand.Next(1,5);
+            if (pos == 1) { x = 90; } else if (pos == 2) { x = 360; } else if (pos == 3) { x = 650; } else if(pos == 4) { x = 917; }
            
             
 
-            int y = 12;
+            int y = -400;
             newEnemy.Location = new Point(x, y);
 
             enemys.Add(newEnemy);
@@ -57,7 +92,7 @@ namespace _2DGame
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
 
 
@@ -86,11 +121,72 @@ namespace _2DGame
            
         }
 
+      
+
+        private void BORDER_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+           
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && (string)x.Tag == "Marking")
+                {
+                    x.Top += speed;
+                }
+
+            }
+            foreach (PictureBox en in marksL.ToList())
+            {
+                if (BORDER_SIGN_LEFT.Bounds.IntersectsWith(en.Bounds))
+                {
+                    marksL.Remove(en);
+                    this.Controls.Remove(en);
+                    amount_signL--;
+                }
+                
+            }
+            foreach (PictureBox en in marksR.ToList())
+            {
+                if (BORDER_SIGN_RIGHT.Bounds.IntersectsWith(en.Bounds))
+                {
+                    marksR.Remove(en);
+                    this.Controls.Remove(en);
+                    amount_signR--;
+                }
+                
+            }
+            this.Invalidate();
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (amount_signL <= 6)
+            {
+                makeMarkL();
+            }
+            if (amount_signR <= 6)
+            {
+                makeMarkR();
+            }
+        }
+
+
         private void timer1_Tick_1(object sender, EventArgs e)
         {
+            score++;
             if (amount_enm <= 3)
             {
                 MakeEnemys();
+                
             }
             if (moveLeft == true && player.Left > 0)
             {
@@ -101,8 +197,8 @@ namespace _2DGame
                 player.Left += speed;
             }
 
+            label1.Text = "Score: " + score;
 
-            this.Invalidate();
 
             foreach (Control x in this.Controls)
             {
@@ -117,7 +213,11 @@ namespace _2DGame
                         gameOver = true;
                     }
                 }
+
+          
+                
             }
+           
             foreach(PictureBox en in enemys.ToList())
             {
                 if (BORDER.Bounds.IntersectsWith(en.Bounds))
